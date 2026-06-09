0457a4dcbf2c485417ec7b9d5c70f3225d0b61d2
Lermistery <sachsuporter@gmail.com>
2026-06-09 09:54:02 +0700

    security: remove sensitive files from tracking (gitignore)

--- backend/spotify_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * spotify_config.php
- * Spotify API Credentials.
- */
-
-define('SPOTIFY_CLIENT_ID', '2a343d8fb9b14312af65ff93d65b530b');
-define('SPOTIFY_CLIENT_SECRET', '9c635c0d86804c9aaccd79945965fdfa');
-?>
